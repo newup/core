427b2d4aa44b967bfb8a225d36d3aaa2febf9a0c
Johnathon Koster <johnathon.koster@paradoxone.com>
2015-06-21 18:38:07 -0500

    Allows custom input drivers

--- src/Console/Kernel.php
+++ src/Console/Kernel.php
@@ -1,11 +1,66 @@
 <?php namespace NewUp\Console;
 
 
+use Symfony\Component\Console\Input\InputDefinition;
+
 class Kernel extends BaseKernel
 {
+
+    /**
+     * Allows a command to override the InputInterface
+     *
+     * @var array
+     */
+    protected $commandInputOverrides = [
+    ];
 
     protected $commands = [
         'NewUp\Console\Commands\About',
     ];
+
+    /**
+     * Determines if a command overrides the input interface.
+     *
+     * @param $command
+     * @return bool
+     */
+    private function commandOverridesInputInterface($command)
+    {
+        return array_key_exists($command, $this->commandInputOverrides);
+    }
+
+    /**
+     * Run the console application.
+     *
+     * @param  \Symfony\Component\Console\Input\InputInterface  $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @return int
+     */
+    public function handle($input, $output = null)
+    {
+        // The name of the command should be the first argument.
+        $commandName = $input->getFirstArgument();
+
+        if ($this->commandOverridesInputInterface($commandName))
+        {
+            $inputClass = $this->commandInputOverrides[$commandName];
+            $input = new $inputClass;
+        }
+
+        try
+        {
+            $this->bootstrap();
+
+            return $this->getArtisan()->run($input, $output);
+        }
+        catch (Exception $e)
+        {
+            $this->reportException($e);
+
+            $this->renderException($output, $e);
+
+            return 1;
+        }
+    }
 
 }
