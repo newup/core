e6a758289c8167396f28acc138cdd41547fbe798
Johnathon Koster <johnathon.koster@paradoxone.com>
2015-07-17 22:17:27 -0500

    Added members and getters
    These will be used by the engine to get the ignore paths, remove paths
    and processing paths. This replaces all the need for the various YAML
    and JSON files. Very nice!

--- src/Templates/BasePackageTemplate.php
+++ src/Templates/BasePackageTemplate.php
@@ -5,4 +5,55 @@ namespace NewUp\Templates;
 abstract class BasePackageTemplate
 {
 
+    /**
+     * The paths that NewUp should ignore.
+     *
+     * @var array
+     */
+    protected $ignoredPaths = [];
+
+    /**
+     * THe paths that NewUp should automatically remove.
+     *
+     * @var array
+     */
+    protected $pathsToRemove = [];
+
+    /**
+     * A list of paths that NewUp should expand and process.
+     *
+     * @var array
+     */
+    protected $pathsToProcess = [];
+
+    /**
+     * Returns the paths that NewUp should ignore.
+     *
+     * @return array
+     */
+    public function getIgnoredPaths()
+    {
+        return $this->ignoredPaths;
+    }
+
+    /**
+     * Returns the paths that NewUp should remove.
+     *
+     * @return array
+     */
+    public function getPathsToRemove()
+    {
+        return $this->ignoredPaths;
+    }
+
+    /**
+     * Returns the paths that NewUp should process.
+     *
+     * @return array
+     */
+    public function getPathsToProcess()
+    {
+        return $this->pathsToProcess;
+    }
+
 }
