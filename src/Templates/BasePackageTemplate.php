20acd5b260a957573a7a41cee376c5240f424bbd
Johnathon Koster <john@stillat.com>
2015-07-24 18:45:05 -0500

    Argument and option helpers

--- src/Templates/BasePackageTemplate.php
+++ src/Templates/BasePackageTemplate.php
@@ -26,6 +26,65 @@ abstract class BasePackageTemplate
      */
     protected $pathsToProcess = [];
 
+
+    /**
+     * A collection of the parsed options.
+     *
+     * @var array
+     */
+    protected $parsedOptions = [];
+
+    /**
+     * A collection of the parsed arguments.
+     *
+     * @var array
+     */
+    protected $parsedArguments = [];
+
+    /**
+     * Get the value of a command option.
+     *
+     * @param      $name
+     * @param null $default
+     * @return mixed
+     */
+    public function option($name, $default = null)
+    {
+        return array_get($this->parsedOptions, $name, $default);
+    }
+
+    /**
+     * Get the value of a command argument.
+     *
+     * @param      $name
+     * @param null $default
+     * @return mixed
+     */
+    public function argument($name, $default = null)
+    {
+        return array_get($this->parsedArguments, $name, $default);
+    }
+
+    /**
+     * Sets the parsed options.
+     *
+     * @param $options
+     */
+    public function setParsedOptions($options)
+    {
+        $this->parsedOptions = $options;
+    }
+
+    /**
+     * Sets the parsed arguments.
+     *
+     * @param $arguments
+     */
+    public function setParsedArguments($arguments)
+    {
+        $this->parsedArguments = $arguments;
+    }
+
     /**
      * Returns the paths that NewUp should ignore.
      *
