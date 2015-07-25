16deb94a8e3a8778214d102a6ea3f814339c4365
Johnathon Koster <johnathon.koster@paradoxone.com>
2015-07-24 20:58:18 -0500

    New method and some comments

--- src/Contracts/Templates/RendererFunction.php
+++ src/Contracts/Templates/RendererFunction.php
@@ -5,8 +5,26 @@ namespace NewUp\Contracts\Templates;
 interface RendererFunction
 {
 
+    /**
+     * Sets the context of the function.
+     *
+     * @param $context
+     * @return mixed
+     */
+    public function setContext($context);
+
+    /**
+     * Gets the name of the function.
+     *
+     * @return string
+     */
     public function getName();
 
+    /**
+     * Gets the function that is passed to the rendering environment.
+     *
+     * @return mixed|\Closure
+     */
     public function getFunction();
 
 }
