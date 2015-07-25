8c58943d0dff1f40e7316b42bf921e9295c7e86a
Johnathon Koster <johnathon.koster@paradoxone.com>
2015-07-24 23:09:25 -0500

    Allows override using constants
    Will be important later

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -1,5 +1,9 @@
 <?php
 
-$loader = require __DIR__ . '/../vendor/autoload.php';
+if (!defined('NEWUP_LOADER_PATH')) {
+    define('NEWUP_LOADER_PATH', __DIR__ . '/../vendor/autoload.php');
+}
+
+$loader = require NEWUP_LOADER_PATH;
 
 return $loader;
