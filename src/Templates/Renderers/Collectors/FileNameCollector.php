5478f141cfdcc5bb5dd0fe5ec57f0fe5660e28a5
Johnathon Koster <john@stillat.com>
2015-07-24 19:12:01 -0500

    Comments

--- src/Templates/Renderers/Collectors/FileNameCollector.php
+++ src/Templates/Renderers/Collectors/FileNameCollector.php
@@ -7,8 +7,22 @@ use NewUp\Contracts\DataCollector;
 class FileNameCollector implements DataCollector
 {
 
+    /**
+     * The collected file names.
+     *
+     * The file names are stored in this array where
+     * the original file name is the key and the
+     * final form of that file path is the value.
+     *
+     * @var array
+     */
     protected $fileNames = [];
 
+    /**
+     * Adds a file name to the list (supplied as an array).
+     *
+     * @param $array
+     */
     public function addFileNames($array)
     {
         $this->fileNames = $this->fileNames + $array;
