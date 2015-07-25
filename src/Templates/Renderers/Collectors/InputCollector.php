dabbd0dfbba80ee1d3ab029fbdbab39c36d758fc
Johnathon Koster <john@stillat.com>
2015-07-24 19:10:49 -0500

    Needed some comments

--- src/Templates/Renderers/Collectors/InputCollector.php
+++ src/Templates/Renderers/Collectors/InputCollector.php
@@ -7,26 +7,56 @@ use NewUp\Contracts\DataCollector;
 class InputCollector implements DataCollector
 {
 
+    /**
+     * The user supplied options.
+     *
+     * @var array
+     */
     protected $options = [];
 
+    /**
+     * The user supplied arguments.
+     *
+     * @var array
+     */
     protected $arguments = [];
 
+    /**
+     * Sets the user supplied arguments.
+     *
+     * @param $args
+     */
     public function setArguments($args)
     {
         $this->arguments = $args;
     }
 
+    /**
+     * Sets the user supplied options.
+     *
+     * @param $opts
+     */
     public function setOptions($opts)
     {
         $this->options = $opts;
     }
 
+    /**
+     * Adds a single argument (supplied as an array).
+     *
+     * @param $array
+     */
     public function addArgument($array)
     {
         $this->arguments = $this->arguments + $array;
     }
 
-    public function addOptioni($array)
+    /**
+     * Adds a single option (supplied as an array).
+     *
+     * @param $array
+     */
+    public function addOption($array)
     {
         $this->options = $this->options + $array;
     }
@@ -43,5 +73,5 @@ class InputCollector implements DataCollector
             'user_args'    => $this->arguments
         ];
     }
-    
+
 }
