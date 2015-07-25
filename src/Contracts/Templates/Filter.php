638181205aa46dd6d228e7162945d36a227e7198
Johnathon Koster <john@stillat.com>
2015-07-24 19:21:16 -0500

    Clearer comments

--- src/Contracts/Templates/Filter.php
+++ src/Contracts/Templates/Filter.php
@@ -5,8 +5,18 @@ namespace NewUp\Contracts\Templates;
 interface Filter
 {
 
+    /**
+     * Gets the name of the filter.
+     *
+     * @return string
+     */
     public function getName();
 
+    /**
+     * Gets the function that is passed to the Twig environment.
+     *
+     * @return \Closure
+     */
     public function getOperator();
 
 }
