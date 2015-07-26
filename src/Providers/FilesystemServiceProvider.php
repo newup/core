1645038a229280b3e420e690d539f7af603e57e2
Johnathon Koster <johnathon.koster@paradoxone.com>
2015-07-25 22:53:36 -0500

    Added binding of TSE

--- src/Providers/FilesystemServiceProvider.php
+++ src/Providers/FilesystemServiceProvider.php
@@ -3,6 +3,7 @@
 namespace NewUp\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use NewUp\Filesystem\TemplateStorageEngine;
 
 class FilesystemServiceProvider extends ServiceProvider
 {
@@ -14,6 +15,9 @@ class FilesystemServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind('NewUp\Contracts\Filesystem\Filesystem', 'NewUp\Filesystem\Filesystem');
+        $this->app->singleton('NewUp\Contracts\Templates\StorageEngine', function() {
+           return new TemplateStorageEngine(app('NewUp\Contracts\Filesystem\Filesystem', template_storage_path()));
+        });
     }
 
 
