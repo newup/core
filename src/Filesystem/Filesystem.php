bf7dfdd163a73aa27453f092efc9a2cc796026b4
Johnathon Koster <john@stillat.com>
2015-08-14 15:18:42 -0500

    Custom implementation of deleteDirectory
    This change will attempt to set the permissions of files that are being
    removed. Quite useful when attempting to brutally remove ".git"
    directories. Windows, man.

--- src/Filesystem/Filesystem.php
+++ src/Filesystem/Filesystem.php
@@ -5,6 +5,7 @@ namespace NewUp\Filesystem;
 use Illuminate\Filesystem\Filesystem as LaravelFileSystem;
 use NewUp\Contracts\Filesystem\Filesystem as FileSystemContract;
 use Symfony\Component\Finder\Finder;
+use FilesystemIterator;
 
 /**
  * Class Filesystem
@@ -47,6 +48,46 @@ class Filesystem extends LaravelFileSystem implements FileSystemContract
         }
 
         return $directories;
+    }
+
+    /**
+     * Recursively delete a directory.
+     *
+     * The directory itself may be optionally preserved.
+     *
+     * @param  string  $directory
+     * @param  bool    $preserve
+     * @return bool
+     */
+    public function deleteDirectory($directory, $preserve = false)
+    {
+        if ( ! $this->isDirectory($directory)) return false;
+
+        $items = new FilesystemIterator($directory);
+
+        foreach ($items as $item)
+        {
+            // If the item is a directory, we can just recurse into the function and
+            // delete that sub-directory otherwise we'll just delete the file and
+            // keep iterating through each file until the directory is cleaned.
+            if ($item->isDir() && ! $item->isLink())
+            {
+                $this->deleteDirectory($item->getPathname());
+            }
+
+            // If the item is just a file, we can go ahead and delete it since we're
+            // just looping through and waxing all of the files in this directory
+            // and calling directories recursively, so we delete the real path.
+            else
+            {
+                @chmod($item->getPathname(), 0755);
+                $this->delete($item->getPathname());
+            }
+        }
+
+        if ( ! $preserve) @rmdir($directory);
+
+        return true;
     }
 
     /**
