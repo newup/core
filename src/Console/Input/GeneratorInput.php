1956d9e9bef302156b5dd0e5f4e4a360756a9c9a
Johnathon Koster <john@stillat.com>
2015-08-15 17:16:42 -0500

    Better error message

--- src/Console/Input/GeneratorInput.php
+++ src/Console/Input/GeneratorInput.php
@@ -90,7 +90,7 @@ class GeneratorInput extends ArgvInput
      *
      * @return string
      */
-    private function getTemplateName()
+    public function getTemplateName()
     {
         if ($this->templateName == null) {
             // The template name should be the second token.
@@ -131,7 +131,7 @@ class GeneratorInput extends ArgvInput
         $includePath = $this->getPackageClassPath();
 
         if (!file_exists($includePath)) {
-            throw new TemplatePackageMissingException("{$includePath} does not exist.");
+            throw new TemplatePackageMissingException("The package template {$this->getTemplateName()} is not installed or it cannot be found. The package template must be installed before it can be built.");
         }
 
         scope_include($includePath);
--- src/Exceptions/TemplatePackageMissingException.php
+++ src/Exceptions/TemplatePackageMissingException.php
@@ -5,4 +5,9 @@ namespace NewUp\Exceptions;
 class TemplatePackageMissingException extends NewUpException
 {
 
+    public function __construct($message = null, $code = 0, \Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
 }
