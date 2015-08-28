e74dab7605b7453fb4df4e6f8e3079ecf069462a
Johnathon Koster <john@stillat.com>
2015-08-28 15:20:29 -0500

    Removes stray string class name

--- src/Templates/Loaders/PackageLoader.php
+++ src/Templates/Loaders/PackageLoader.php
@@ -5,6 +5,7 @@ namespace NewUp\Templates\Loaders;
 use NewUp\Contracts\Filesystem\Filesystem;
 use NewUp\Exceptions\InvalidPathException;
 use NewUp\Exceptions\NewUpException;
+use NewUp\Templates\BasePackageTemplate;
 use NewUp\Templates\Package;
 
 class PackageLoader
@@ -39,33 +40,34 @@ class PackageLoader
             throw new InvalidPathException("The directory {$directory} does not exist.");
         }
 
-        if (!$this->files->exists($directory.'/composer.json')) {
+        if (!$this->files->exists($directory . '/composer.json')) {
             throw new InvalidPathException("There is no composer.json file in {$directory}");
         }
 
-        if (!$this->files->exists($directory.'/_newup')) {
+        if (!$this->files->exists($directory . '/_newup')) {
             throw new InvalidPathException("There is no _newup directory in {$directory}");
         }
 
-        if (!$this->files->exists($directory.'/_newup/Package.php')) {
+        if (!$this->files->exists($directory . '/_newup/Package.php')) {
             throw new InvalidPathException("A Package.php file must be present in in \"_newup\" directory.");
         }
 
-        $package = Package::fromFile($directory.'/composer.json', user_config('configuration.strictComposerValues', true));
+        $package = Package::fromFile($directory . '/composer.json',
+            user_config('configuration.strictComposerValues', true));
         $namespace = package_vendor_namespace($package->getVendor(), $package->getPackage(), true);
 
-        add_psr4($namespace, $directory.'/_newup');
+        add_psr4($namespace, $directory . '/_newup');
 
 
-        if (!class_exists($namespace.'Package', true)) {
+        if (!class_exists($namespace . 'Package', true)) {
             throw new NewUpException("A valid reachable class named 'Package' must be defined.");
         }
 
-        if (!is_subclass_of($namespace.'Package', 'NewUp\\Templates\\BasePackageTemplate')) {
-            throw new NewUpException("The 'Package' class must extend 'NewUp\\Templates\\BasePackageTemplate'");
+        if (!is_subclass_of($namespace . 'Package', BasePackageTemplate::class)) {
+            throw new NewUpException("The 'Package' class must extend ".BasePackageTemplate::class);
         }
 
-        return $namespace.'Package';
+        return $namespace . 'Package';
     }
 
 }
