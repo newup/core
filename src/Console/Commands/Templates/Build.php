4731e29e8a19b98cbf17bcfb9ccd55a8f2be3186
Johnathon Koster <john@stillat.com>
2015-09-07 15:19:09 -0500

    No arguments can be optional at this time
    NewUp cannot have optional arguments at this time in case a package
    template defines required arguments.

--- src/Console/Commands/Templates/Build.php
+++ src/Console/Commands/Templates/Build.php
@@ -67,9 +67,8 @@ class Build extends Command
             ['newup-template', InputArgument::REQUIRED, 'The template name', null],
             [
                 'newup-output-directory',
-                InputArgument::OPTIONAL,
-                'The directory the built template should be saved to',
-                '.'
+                InputArgument::REQUIRED,
+                'The directory the built template should be saved to'
             ]
         ];
     }
