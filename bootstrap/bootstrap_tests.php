eb0b6ab30f4de300880e9d673e38af5663fde6a4
Johnathon Koster <john@stillat.com>
2015-09-12 17:55:32 -0500

    New test helper to get fixture file path

--- bootstrap/bootstrap_tests.php
+++ bootstrap/bootstrap_tests.php
@@ -4,6 +4,18 @@ include 'autoload.php';
 
 include 'app.php';
 
+if (!function_exists('getFixturePath')) {
+    /**
+     * Returns the full path of a fixture file.
+     *
+     * @param $file
+     * @return string
+     */
+    function getFixturePath($file) {
+        return realpath(__DIR__.'/../tests/fixtures/'.$file);
+    }
+}
+
 if (!function_exists('loadFixtureContent')) {
     /**
      * Returns the content of a given fixture file.
@@ -12,7 +24,7 @@ if (!function_exists('loadFixtureContent')) {
      * @return string
      */
     function loadFixtureContent($file) {
-        return file_get_contents(realpath(__DIR__.'/../tests/fixtures/'.$file));
+        return file_get_contents(getFixturePath($file));
     }
 }
 
