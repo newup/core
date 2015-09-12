e1d193cd0ebef2b3dea6b71bc4eefaf55d41fc74
Johnathon Koster <john@stillat.com>
2015-09-12 18:09:45 -0500

    Added new helper to normalize line ending

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -238,3 +238,23 @@ if (!function_exists('remove_ansi')) {
         return $ansi->colorStrip($input);
     }
 }
+
+if (!function_exists('normalize_line_endings')) {
+    /**
+     * Normalizes line endings.
+     *
+     * Adapted from
+     * https://www.darklaunch.com/2009/05/06/php-normalize-newlines-line-endings-crlf-cr-lf-unix-windows-mac
+     * 
+     * @param $string
+     */
+    function normalize_line_endings($string) {
+        // Normalize line endings using Global
+        // Convert all line-endings to UNIX format
+        $string = str_replace("\r\n", "\n", $string);
+        $string = str_replace("\r", "\n", $string);
+        // Don't allow out-of-control blank lines
+        $string = preg_replace("/\n{2,}/", "\n" . "\n", $string);
+        return $string;
+    }
+}
