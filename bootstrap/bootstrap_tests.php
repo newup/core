c84e79e3e0aec862044a7382304999a16ec95099
Johnathon Koster <john@stillat.com>
2015-09-12 17:53:28 -0500

    Adds a helper function to load test fixture contents

--- bootstrap/bootstrap_tests.php
+++ bootstrap/bootstrap_tests.php
@@ -4,5 +4,17 @@ include 'autoload.php';
 
 include 'app.php';
 
+if (!function_exists('loadFixtureContent')) {
+    /**
+     * Returns the content of a given fixture file.
+     *
+     * @param $file
+     * @return string
+     */
+    function loadFixtureContent($file) {
+        return file_get_contents(realpath(__DIR__.'/../tests/fixtures/'.$file));
+    }
+}
+
 $kernel = $app->make('Illuminate\Contracts\Console\Kernel');
 $kernel->bootstrap();
