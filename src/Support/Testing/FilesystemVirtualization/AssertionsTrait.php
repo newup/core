585703659b86b20215d7cdf125a58588992be111
Johnathon Koster <john@stillat.com>
2015-09-14 17:10:43 -0500

    Provides a vfs wrapper around PHPUnit file assertions

---
+++ src/Support/Testing/FilesystemVirtualization/AssertionsTrait.php
@@ -0,0 +1,241 @@
+<?php
+
+namespace NewUp\Support\Testing\FilesystemVirtualization;
+
+use PHPUnit_Framework_Assert as PHPUnit;
+
+trait AssertionsTrait
+{
+
+    /**
+     * Get the virtualized path for a given path.
+     *
+     * @param $path
+     * @return mixed
+     */
+    public function getPath($path)
+    {
+        return $path;
+    }
+
+    /**
+     * Asserts that the contents of one vfs file is equal to the contents of another
+     * vfs file.
+     *
+     * @param $expected
+     * @param $actual
+     * @param string $message
+     * @param bool|false $canonicalize
+     * @param bool|false $ignoreCase
+     */
+    public function assertVfsFileEquals($expected, $actual, $message = '', $canonicalize = false, $ignoreCase = false)
+    {
+        PHPUnit::assertFileEquals($this->getPath($expected), $this->getPath($actual), $message, $canonicalize,
+            $ignoreCase);
+    }
+
+    /**
+     * Asserts that the contents of one vfs file is not equal to the contents of
+     * another vfs file.
+     *
+     * @param $expected
+     * @param $actual
+     * @param string $message
+     * @param bool|false $canonicalize
+     * @param bool|false $ignoreCase
+     */
+    public function assertVfsFileNotEquals(
+        $expected,
+        $actual,
+        $message = '',
+        $canonicalize = false,
+        $ignoreCase = false
+    ) {
+        PHPUnit::assertFileNotEquals($this->getPath($expected), $this->getPath($actual), $message, $canonicalize,
+            $ignoreCase);
+    }
+
+    /**
+     * Asserts that the contents of a string is equal
+     * to the contents of a vfs file.
+     *
+     * @param $expectedFile
+     * @param $actualString
+     * @param string $message
+     * @param bool|false $canonicalize
+     * @param bool|false $ignoreCase
+     */
+    public function assertStringEqualsVfsFile(
+        $expectedFile,
+        $actualString,
+        $message = '',
+        $canonicalize = false,
+        $ignoreCase = false
+    ) {
+        PHPUnit::assertStringEqualsFile($expectedFile, $actualString, $message, $canonicalize, $ignoreCase);
+    }
+
+    /**
+     * Asserts that the contents of a string is not equal
+     * to the contents of a vfs file.
+     *
+     * @param $expectedFile
+     * @param $actualString
+     * @param string $message
+     * @param bool|false $canonicalize
+     * @param bool|false $ignoreCase
+     */
+    public function assertStringNotEqualsVfsFile(
+        $expectedFile,
+        $actualString,
+        $message = '',
+        $canonicalize = false,
+        $ignoreCase = false
+    ) {
+        PHPUnit::assertStringNotEqualsFile($this->getPath($expectedFile), $actualString, $message, $canonicalize,
+            $ignoreCase);
+    }
+
+    /**
+     * Asserts that a vfs file exists.
+     *
+     * @param $filename
+     * @param string $message
+     */
+    public function assertVfsFileExists($filename, $message = '')
+    {
+        PHPUnit::assertFileExists($this->getPath($filename), $message);
+    }
+
+    /**
+     * Asserts that a vfs file does not exists.
+     *
+     * @param $filename
+     * @param string $message
+     */
+    public function assertVfsFileNotExists($filename, $message = '')
+    {
+        PHPUnit::assertFileNotExists($this->getPath($filename), $message);
+    }
+
+    /**
+     * Asserts that a string matches a given format file.
+     *
+     * @param $formatFile
+     * @param $string
+     * @param string $message
+     */
+    public function assertStringMatchesVfsFormatFile($formatFile, $string, $message = '')
+    {
+        PHPUnit::assertStringMatchesFormatFile($this->getPath($formatFile), $string, $message);
+    }
+
+    /**
+     * Asserts that a string does not match a given format string.
+     *
+     * @param $formatFile
+     * @param $string
+     * @param string $message
+     */
+    public function assertStringNotMatchesVfsFormatFile($formatFile, $string, $message = '')
+    {
+        PHPUnit::assertStringNotMatchesFormatFile($this->getPath($formatFile), $string, $message);
+    }
+
+    /**
+     * Asserts that two vfs XML files are equal.
+     *
+     * @param $expectedFile
+     * @param $actualFile
+     * @param string $message
+     */
+    public function assertVfsXmlFileEqualsVfsXmlFile($expectedFile, $actualFile, $message = '')
+    {
+        PHPUnit::assertXmlFileEqualsXmlFile($this->getPath($expectedFile), $this->getPath($actualFile), $message);
+    }
+
+    /**
+     * Asserts that two vfs XML files are not equal.
+     *
+     * @param $expectedFile
+     * @param $actualFile
+     * @param string $message
+     */
+    public function assertXmlFileNotEqualsXmlFile($expectedFile, $actualFile, $message = '')
+    {
+        PHPUnit::assertXmlFileNotEqualsXmlFile($this->getPath($expectedFile), $this->getPath($actualFile), $message);
+    }
+
+    /**
+     * Asserts that two vfs XML documents are equal.
+     *
+     * @param $expectedFile
+     * @param $actualXml
+     * @param string $message
+     */
+    public function assertXmlStringEqualsVfsXmlFile($expectedFile, $actualXml, $message = '')
+    {
+        PHPUnit::assertXmlStringEqualsXmlFile($this->getPath($expectedFile), $actualXml, $message);
+    }
+
+    /**
+     * Asserts that two vfs XML documents are not equal.
+     *
+     * @param $expectedFile
+     * @param $actualXml
+     * @param string $message
+     */
+    public function assertXmlStringNotEqualsXmlFile($expectedFile, $actualXml, $message = '')
+    {
+        PHPUnit::assertXmlStringNotEqualsXmlFile($this->getPath($expectedFile), $actualXml, $message);
+    }
+
+    /**
+     * Asserts that the generated JSON encoded object and the content of the given vfs file are equal.
+     *
+     * @param $expectedFile
+     * @param $actualJson
+     * @param string $message
+     */
+    public function assertJsonStringEqualsVfsJsonFile($expectedFile, $actualJson, $message = '')
+    {
+        PHPUnit::assertJsonStringEqualsJsonFile($this->getPath($expectedFile), $actualJson, $message);
+    }
+
+    /**
+     * Asserts that the generated JSON encoded object and the content of the given vfs file are not equal.
+     *
+     * @param $expectedFile
+     * @param $actualJson
+     * @param string $message
+     */
+    public function assertJsonStringNotEqualsVfsJsonFile($expectedFile, $actualJson, $message = '')
+    {
+        PHPUnit::assertJsonStringNotEqualsJsonFile($this->getPath($expectedFile), $actualJson, $message);
+    }
+
+    /**
+     *  Asserts that two vfs JSON files are equal.
+     *
+     * @param $expectedFile
+     * @param $actualFile
+     * @param string $message
+     */
+    public function assertVfsJsonFileEqualsVfsJsonFile($expectedFile, $actualFile, $message = '')
+    {
+        PHPUnit::assertJsonFileEqualsJsonFile($this->getPath($expectedFile), $this->getPath($actualFile), $message);
+    }
+
+    /**
+     * Asserts that two vfs JSON files are not equal.
+     *
+     * @param $expectedFile
+     * @param $actualFile
+     * @param string $message
+     */
+    public function assertVfsJsonFileNotEqualsVfsJsonFile($expectedFile, $actualFile, $message = '')
+    {
+        PHPUnit::assertJsonFileNotEqualsJsonFile($this->getPath($expectedFile), $this->getPath($actualFile), $message);
+    }
+
+}
