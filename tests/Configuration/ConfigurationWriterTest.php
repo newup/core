3c0afeb27f4c5c55a14382672a77605a6d82272a
Johnathon Koster <johnathon.koster@paradoxone.com>
2015-09-13 21:03:50 -0500

    Added file system tests

--- tests/Configuration/ConfigurationWriterTest.php
+++ tests/Configuration/ConfigurationWriterTest.php
@@ -1,9 +1,12 @@
 <?php
 
 use NewUp\Configuration\ConfigurationWriter;
+use NewUp\Support\Testing\FilesystemVirtualization\FilesystemVirtualization;
 
 class ConfigurationWriterTest extends PHPUnit_Framework_TestCase
 {
+
+    use FilesystemVirtualization;
 
     /**
      * The ConfigurationWriter instance.
@@ -12,6 +15,8 @@ class ConfigurationWriterTest extends PHPUnit_Framework_TestCase
      */
     private $writer;
 
+    protected $virtualPath = 'config';
+
     public function setUp()
     {
         $this->writer = new ConfigurationWriter([
@@ -19,6 +24,12 @@ class ConfigurationWriterTest extends PHPUnit_Framework_TestCase
             'second' => 'The Second',
             'third' => 'The Third'
         ]);
+        $this->setUpVfs();
+    }
+
+    public function tearDown()
+    {
+        $this->tearDownVfs();
     }
 
     public function testRestClearsConfigurationItems()
@@ -29,22 +40,26 @@ class ConfigurationWriterTest extends PHPUnit_Framework_TestCase
 
     public function testSaveWritesFilesToSystem()
     {
-
+        $this->writer->save($this->getPath('test.json'));
+        $this->assertFileExists($this->getPath('test.json'));
     }
 
     public function testSaveWritesCorrectJson()
     {
-
+        $this->writer->save($this->getPath('test.json'));
+        $this->assertEquals(loadFixtureContent('Configuration/expected.json'), $this->getContents('test.json'));
     }
 
     public function testSaveYamlWritesFilesToSystem()
     {
-
+        $this->writer->saveYaml($this->getPath('test.yaml'));
+        $this->assertFileExists($this->getPath('test.yaml'));
     }
 
     public function testSaveYamlWritesCorrectYaml()
     {
-
+        $this->writer->saveYaml($this->getPath('test.yaml'));
+        $this->assertEquals(loadFixtureContent('Configuration/expected.yaml'), $this->getContents('test.yaml'));
     }
 
 }
