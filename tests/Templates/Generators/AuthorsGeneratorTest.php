c0c2be8d8f2a7ff3ad424c159cd5e394ecc958a8
Johnathon Koster <johnathon.koster@paradoxone.com>
2015-09-14 19:13:54 -0500

    Adds AuthorsGeneratorTest
    Uses ::Class notation for stdClass, which differs from old test

---
+++ tests/Templates/Generators/AuthorsGeneratorTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use NewUp\Templates\Generators\AuthorsGenerator;
+
+class AuthorsGeneratorTest extends \PHPUnit_Framework_TestCase
+{
+
+    protected $authors = [
+        ['name' => 'Johnathon Koster', 'email' => 'john@stillat.com'],
+        ['name' => 'Example', 'email' => 'user@example.com']
+    ];
+
+    public function testGeneratorConvertsNestedArraysToObjects()
+    {
+        $authors = AuthorsGenerator::generate($this->authors);
+
+        foreach ($authors as $author) {
+            $this->assertInstanceOf(\stdClass::class, $author);
+        }
+    }
+
+
+}
