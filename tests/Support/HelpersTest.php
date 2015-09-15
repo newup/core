9b4eff5885dba58d3d2d09ebc0c14061ddbb9e08
Johnathon Koster <johnathon.koster@paradoxone.com>
2015-09-14 21:13:46 -0500

    Added more helpers tests

--- tests/Support/HelpersTest.php
+++ tests/Support/HelpersTest.php
@@ -1,5 +1,7 @@
 <?php namespace NewUp\Tests\Support;
 
+use Illuminate\Contracts\Config\Repository as ConfigRepository;
+
 class HelpersTest extends \PHPUnit_Framework_TestCase
 {
 
@@ -13,6 +15,39 @@ class HelpersTest extends \PHPUnit_Framework_TestCase
         array_remove_value($array, 'test2');
 
         $this->assertEquals('test', $array[0]);
+    }
+
+    public function testGetUserConfigPathReturnsDefault()
+    {
+        $value = get_user_config_path();
+        $this->assertEquals(config_path('user'), $value);
+    }
+
+    public function testGetUserConfigObservesConfigConstant()
+    {
+        define('NEWUP_CORE_USER_CONFIGURATION_DIRECTORY', 'test');
+        $this->assertEquals('test', get_user_config_path());
+    }
+
+    public function testUserConfigReturnsConfigRepository()
+    {
+        $instance = user_config();
+        $this->assertInstanceOf(ConfigRepository::class, $instance);
+    }
+
+    public function testUserConfigSettingAndGettingWorks()
+    {
+        user_config([
+           'test' => 'test_value'
+        ]);
+
+        $this->assertEquals('test_value', user_config('test'));
+    }
+
+    public function testLoadSystemTemplateReturnsCorrectContent()
+    {
+        $content = load_system_template('Example');
+        $this->assertEquals('{# This is just an example. Do not use. #}', $content);
     }
 
 }
