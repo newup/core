9c07ad274a3ae4271c0c514faf1246b3b14a14e9
Johnathon Koster <john@stillat.com>
2015-09-16 03:25:10 -0500

    Uses ::class notation

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,13 +5,13 @@ $app = new NewUp\Foundation\Application(realpath(__DIR__ . '/../'));
 $app->useStoragePath(realpath(__DIR__ . '/../storage/'));
 
 $app->singleton(
-    'Illuminate\Contracts\Console\Kernel',
-    'NewUp\Console\Kernel'
+    \Illuminate\Contracts\Console\Kernel::class,
+    \NewUp\Console\Kernel::class
 );
 
 $app->singleton(
-    'Illuminate\Contracts\Debug\ExceptionHandler',
-    'NewUp\Exceptions\Handler'
+    \Illuminate\Contracts\Debug\ExceptionHandler::class,
+    \NewUp\Exceptions\Handler::class
 );
 
 return $app;
